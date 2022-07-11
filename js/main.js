ca4f8e2b78cebfe47156ac28cf586db6731c7913
John Runyard <jrunyard3ft@gmail.com>
2022-07-11 10:12:52 -0500

    Added form, tweaked some content.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
-$(document).ready(function(){
-    $('.hero-slider').slick({
-        slidesToShow: 1,
+$(document).ready(function () {
+  $('.hero-slider').slick({
+    slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay:false,
-    dots:true,
-   nav:false,
-   prevArrow: '<span class="prev-arrow3" ><i class="fal fa-chevron-left"></i></span>',
-     nextArrow: '<span class="next-arrow3" ><i class="fal fa-chevron-right"></i></span>' 
-    });
+    autoplay: false,
+    dots: true,
+    nav: false,
+    prevArrow: '<span class="prev-arrow3" ><i class="fal fa-chevron-left"></i></span>',
+    nextArrow: '<span class="next-arrow3" ><i class="fal fa-chevron-right"></i></span>'
+  });
 })
 
 
-$(document).ready(function(){
-    $('.media-slider').slick({
-    
-        slidesToShow: 1,
+$(document).ready(function () {
+  $('.media-slider').slick({
+
+    slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay:false,
-    dots:true,  
-    });
+    autoplay: false,
+    dots: true,
+  });
 })
 
 
-$(document).ready(function(){
-    $('.award-slider').slick({
-        slidesToShow: 3,
+$(document).ready(function () {
+  $('.award-slider').slick({
+    slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay:false,
-    dots:false,
+    autoplay: false,
+    dots: false,
     prevArrow: '<span class="prev-arrow3" ><i class="fa fa-chevron-left"></i></span>',
-     nextArrow: '<span class="next-arrow3" ><i class="fa fa-chevron-right"></i></span>' 
-    });
+    nextArrow: '<span class="next-arrow3" ><i class="fa fa-chevron-right"></i></span>'
+  });
 })
 
 // function myFunction(x) {
@@ -40,26 +40,43 @@ $(document).ready(function(){
 
 
 // (function ($) { // Begin jQuery
-    $(document).ready(function() { // DOM ready
-      // If a link has a dropdown, add sub menu toggle.
-      $('nav ul li a:not(:only-child)').click(function(e) {
-        $(this).siblings('.nav-dropdown').toggle();
-        // Close one dropdown when selecting another
-        $('.nav-dropdown').not($(this).siblings()).hide();
-        e.stopPropagation();
-      });
-      // Clicking away from dropdown will remove the dropdown class
-      $('html').click(function() {
-        $('.nav-dropdown').hide();
-      });
-      // Toggle open and close nav styles on click
-      $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
-      });
-      // Hamburger to X toggle
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active');
-      });
-    }); // end DOM ready
+$(document).ready(function () { // DOM ready
+  // If a link has a dropdown, add sub menu toggle.
+  $('nav ul li a:not(:only-child)').click(function (e) {
+    $(this).siblings('.nav-dropdown').toggle();
+    // Close one dropdown when selecting another
+    $('.nav-dropdown').not($(this).siblings()).hide();
+    e.stopPropagation();
+  });
+  // Clicking away from dropdown will remove the dropdown class
+  $('html').click(function () {
+    $('.nav-dropdown').hide();
+  });
+  // Toggle open and close nav styles on click
+  $('#nav-toggle').click(function () {
+    $('nav ul').slideToggle();
+  });
+  // Hamburger to X toggle
+  $('#nav-toggle').on('click', function () {
+    this.classList.toggle('active');
+  });
+  // $("#staticform").submit(function (event) {
+  //   event.preventDefault();
+  //   $.ajax({
+  //     url: 'https://api.staticforms.xyz/submit', // url where to submit the request
+  //     type: "POST", // type of action POST || GET
+  //     dataType: 'json', // data type
+  //     data: $("#staticform").serialize(), // post data || get data
+  //     success: function (result) {
+  //       // you can see the result from the console
+  //       // tab of the developer tools
+  //       alert(JSON.parse(result));
+  //     },
+  //     error: function (xhr, resp, text) {
+  //       alert(xhr, resp, text);
+  //     }
+  //   })
+  // });
+}); // end DOM ready
 //   })
 (jQuery); // end jQuery
